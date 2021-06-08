a75187cc61aa398ef269257509ecacb5bfb62fc9
chimado <ido.ludmer@gmail.com>
2021-06-08 19:43:51 +0300

    comms_wip

--- bt/bt.ino
+++ bt/bt.ino
@@ -1,4 +1,5 @@
-String incoming_value =  "";
+String incoming_string =  "";
+char incoming_char = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -8,28 +9,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  incoming_value = String (Serial.read());
-  
-  if (incoming_value != "-1"){
-    Serial.println(rd());
+  incoming_string = String (Serial.read());
+  incoming_char = char (Serial.read());
+    
+  if (incoming_string != "-1"){
+    Serial.println(incoming_char);
   }
-}
-
-String  rd(){ // string array, it needs to be dynamic and increase its size with each loop, maybe create two arrays and each loop copy one to the other and remake the one copied from but with an additional cell
-  String* msg1 = new String[1]{ "0" };
-  String* msg2 = new String[1]{ "0" };
-  String ans = "";
-  
-  while (incoming_value != "-1"){ // inserts the last value of incoming_value to the array, then checks if the next value is -1, if not the loop runs again with the value gathered at its end
-    msg1[0] = msg2[0];
-    msg2[0] = incoming_value;
-    incoming_value = String (Serial.read());
-    Serial.println(incoming_value);
-  }
-  // here you need to either convert it all from binary to normal letters, do it with ascii conversion, also the array of strings needs to be made into a single string for the function to output
-  Serial.println("1: " + msg1[0]);
-  Serial.println("2: "+ msg2[0]);
-
-  ans = "ans: " + msg1[0] + msg2[0];
-  return ans;
 }
