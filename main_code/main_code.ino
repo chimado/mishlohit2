4e4dc90d80e15c6d5d5d7d914a2c9e5e3a1505fd
chimado <ido.ludmer@gmail.com>
2021-06-13 18:38:57 +0300

    phase_1_complete

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,37 +1,59 @@
 // libraries
 #include <Servo.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
 /*
 bluetooth communications dictionary:
 c - connection request
 p - password attempt
-t - trunk request (0 afterwards for close or 1 for open)
+t - trunk request (0  for closing it and 1 for opening it)
 g - gps location incoming
 d - drive request
+h - help
  */
 
 // misc
 Servo trunk; // creates servo object to control trunk
 Servo steering; // creates servo object to control steering
+TinyGPS gps; // creates gps object
+SoftwareSerial ss(10, 9); // sets gps Tx to 9 and Rx to 10
 
 //  variables
 int phase = 0; // 0 is start of drive, 1 is navigation, 2 is end of drive
 bool authorized = false; // indicates if the connected device is authorized
 
+// these store the gps coordinates, lat - latitude lon - longitude t - target o - origin
+float tlat, tlon, olat, olon;
+
 // IO pins
 const int trunkPin = 3;
 const int steeringPin = 5;
+const int flir = 0;
+const int frir = 1;
+const int lir = 2;
+const int rir = 3;
+const int motorb = 7;
+const int motorf = 8;
+const int mpwm = 6;
 
 void setup() {
-  Serial.begin(9600); // sets baud rate for arduino
-
+  Serial.begin(9600); // sets baud rate for arduino serial
+  ss.begin(9600); // sets baud rate for gps serial
+  
   // pin attachment
   trunk.attach(trunkPin); // attaches the selected pin to the trunk servo object
   steering.attach(steeringPin); // attaches the selected pin to the steering servo object
+  pinMode(motorf, OUTPUT);
+  pinMode(motorb, OUTPUT);
+  pinMode(mpwm, OUTPUT);
 
   // set initial values
   trunk.write(0);
   steering.write(90);
+  digitalWrite(motorf, LOW);
+  digitalWrite(motorb, HIGH);
+  sstop();
 }
 
 void loop() {
@@ -39,6 +61,32 @@ void loop() {
   if (phase == 0){
     stod();
   }
+
+  else if (phase == 1){
+    nav();
+  }
+}
+
+void nav(){
+   
+}
+
+void drive(int d, int p){ // use drive(direction{f or b}, power {0-255})
+  analogWrite(mpwm, p);
+    
+  if (d == 0){
+    digitalWrite(motorf, LOW);
+    digitalWrite(motorb, HIGH);
+  }
+
+  else{
+    digitalWrite(motorf, HIGH);
+    digitalWrite(motorb, LOW);
+  }
+}
+
+void sstop(){
+  analogWrite(mpwm, 0);
 }
 
 // the start of drive phase function, it's responsible for phase 1
@@ -52,18 +100,159 @@ void stod(){
     }
 
     else if (lastMessage == "g"){
-      Serial.println("recieving gps coordinates");
+      Serial.println("recieving gps coordinates, send the latitude before the longitude"); // gps has 6 numbers after the decimal place
+      readGPS();
     }
 
-    else if (lastMessage == "d"){
-      Serial.println("preparing to drive");
+    else if (lastMessage == "d"){ // it needs to save its current location in order to come back to it later
+      Serial.println("are you sure you want to start driving? type yes if you're sure or anything else if you're not");
+      Serial.println("warning: there is no way to reverse this action, type yes only if you're absolutely sure you want the drive to start");
+
+      bool confirm = true;
+
+      for (int i = 0; i < 3; i++){
+        switch(i){
+          case 0:
+            if (waitForInput() != "y"){
+              confirm = false;
+            }
+            break;
+
+            case 1:
+            if (waitForInput() != "e"){
+              confirm = false;
+            }
+            break;
+
+            case 2:
+            if (waitForInput() != "s"){
+              confirm = false;
+            }
+            break;
+        }
+      }
+      
+      if (confirm = true){
+        Serial.println("preparing to drive");
+        trunkState("0");
+        getGPS();
+
+        if (spaceForDriveStart() == true){
+          phase = 1;
+        }
+
+        else{
+          Serial.println("error - not enough space in front to start drive");
+        }
+      }
+    }
+
+    else if (lastMessage == "h"){
+      Serial.println("c - connection request");
+      Serial.println(" p - password attempt");
+      Serial.println(" t - trunk request (0 for closing it and 1 for opening it)");
+      Serial.println("g - gps location incoming");
+      Serial.println("d - drive request");
     }
 
     else{
       Serial.println("invalid input");
     }
+  }
+}
+
+bool spaceForDriveStart(){
+  if (getFLIR() < 30.00 && getFRIR < 30.00){ // checks if there's any object up to 30 cm in front
+    return false;
+  }
+
+  else{
+    return true;
+  }
+}
+
+// all get IR functions operate on the same principles
+// it multiplies the analog input of the selected IR sensor (front left, front right, left and right) by several constants
+// 
+float getFLIR(){
+  float distance =  11.58 * pow(analogRead(flir)*0.0048828125, -1.10);
+
+  return distance;
+}
+
+float getFRIR(){
+  float distance =  11.58 * pow(analogRead(frir)*0.0048828125, -1.10);
+
+  return distance;
+}
+
+float getLIR(){
+  float distance =  11.58 * pow(analogRead(lir)*0.0048828125, -1.10);
+
+  return distance;
+}
+
+float getRIR(){
+  float distance =  11.58 * pow(analogRead(rir)*0.0048828125, -1.10);
+
+  return distance;
+}
+
+// gets the GPS coordniates from the app, and sets tlat and tlon to the correct coordinates
+void readGPS(){
+  float temp = 0; // stores the coordinates temporarily while they're being retrived
+  String temps = ""; // stores the bluetooth message temporarily
+  
+  for (int z = 0; z < 2; z++){
+    for (int i = 0; i < 6; i++){
+      temps = waitForInput();
+
+      if (temps != "."){
+        temp = temp + float(temps.toInt()) * pow(10.00, -float(i));
+      }
+
+      else{
+        i = i - 1;
+      }
+    }
+
+    if (z == 0){
+      tlat = temp;
+      temp = 0;
+    }
     
-    
+    else{
+      tlon = temp;
+    }
+  }
+}
+
+// gets the GPS coordinates from the GPS module
+void getGPS(){
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (ss.available())
+    {
+      char c = ss.read();
+      if (gps.encode(c)) // Did a new valid sentence come in?
+        newData = true;
+    }
+  }
+
+  if (newData)
+  {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+
+    // sets the global variables to the current location
+    olat = flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6;
+    olon = flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6;
   }
 }
 
@@ -74,12 +263,12 @@ void trunkState(String in){
 
     switch(state){
       case 0:
-        trunk.write(0);
+        trunk.write(180);
         Serial.println("closing trunk");
         break;
         
       case 1:
-        trunk.write(180);
+        trunk.write(0);
         Serial.println("opening  trunk");
         break;  
         
