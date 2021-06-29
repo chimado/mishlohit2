4b0354e67f6d1cedf1a5d1ae0568ad0a6d9ddbf9
chimado <ido.ludmer@gmail.com>
2021-06-29 12:51:24 +0300

    possible ir fix2

---
+++ ir_sensor2/ir_sensor.ino
@@ -0,0 +1,30 @@
+int IRpin = 1;  
+float ch = 60;/*
+int counter = 0;
+float pvalue = 0;*/ 
+                                  
+void setup() {
+  Serial.begin(9600);                             
+}
+
+void loop() { // 15 11.58
+  float volts = analogRead(IRpin)*0.0048828125;   
+  float distance = 11.58 * pow(volts, -1.10);
+  Serial.println(distance);
+  /*
+  counter = counter + 1;
+  if (abs(distance * ch - 10) < abs(distance * pvalue - 10)){
+    pvalue = (pvalue + ch) / 2;
+  }
+
+  else {
+    ch = (pvalue + ch) / 2;
+  }
+
+  if (counter == 10){
+    Serial.println(ch);
+    counter = 0;
+  }
+  */
+  delay(100);                                     
+}
